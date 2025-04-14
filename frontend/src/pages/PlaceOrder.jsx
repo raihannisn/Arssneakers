344f723263f18b59bc81134a03306e34322ff988
raihannisn <raihannurinsan@gmail.com>
2025-04-14 20:20:13 +0700

    Cart

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Title from '../components/Title'
+import CartTotal from '../components/CartTotal'
 
 const PlaceOrder = () => {
   return (
@@ -7,11 +8,35 @@ const PlaceOrder = () => {
 
       {/* ------- Left Side ------- */}
       <div className='flex flex-col gap-4 w-full sm:max-w-[480px]'>
-        <div className='text-xl sm:text-2xl my-3'>
+        <div className='text-xl sm:text-2xl my-3 pt-10'>
           <Title text1={'DELIVERY'} text2={'INFORMATION'}/>
         </div>
         <div className='flex gap-3'>
+          <input type="text" placeholder='First Name' className=' border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+          <input type="text" placeholder='Last Name' className=' border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+        </div>
+        <input type="email" placeholder='Email Address' className=' border border-gray-300 rounded py-1.5 px-3.5 w-full' />        
+        <input type="email" placeholder='Street' className=' border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+        <div className='flex gap-3'>
+          <input type="text" placeholder='City' className=' border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+          <input type="text" placeholder='State' className=' border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+        </div>
+        <div className='flex gap-3'>
+          <input type="number" placeholder='Zipcode' className=' border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+          <input type="text" placeholder='Country' className=' border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+        </div>
+        <input type="number" placeholder='Phone' className=' border border-gray-300 rounded py-1.5 px-3.5 w-full' />
+      </div>
+      {/* ------- Right Side ------- */}
+      <div className='mt-8'>
+        <div className='mt-8 min-w-80'>
+          <CartTotal/>
+        </div>
+        <div className='mt-12'>
+        <Title text1={'PAYMENT'} text2={'METHOD'}/>
+        <div className='flex gap-3 flex-col lg:flex-row'>
 
+        </div>
         </div>
       </div>
     </div>
