2810d5e98a63db981317c190bab824128623dec9
raihannisn <raihannurinsan@gmail.com>
2025-04-24 23:29:34 +0700

    Update Server

---
+++ backend/controllers/productController.js
@@ -0,0 +1,43 @@
+
+
+// function for add product
+const addProduct = async (req, res) => {
+    try {
+        const { name, description, price, category, subCategory, sizes, bestSeller } = req.body
+
+        const image1 = req.files.image1[0].path
+        const image2 = req.files.image2[0].path
+        const image3 = req.files.image3[0].path
+        const image4 = req.files.image4[0].path
+
+        console.log( name, description, price, category, subCategory, sizes, bestSeller )
+        console.log( image1, image2, image3, image4 )
+
+        res.json({})
+        
+    } catch (error) {
+        console.log(error);~
+        req.json({ success: false, message: error.message })        
+    }
+    
+}
+
+// function for list product
+const listProduct = async (req, res) => {
+    
+
+}
+
+// function for remove product
+const removeProduct = async (req, res) => {
+    
+
+}
+
+// function for single product info
+const singleProduct = async (req, res) => {
+    
+
+}
+
+export { addProduct, listProduct, removeProduct, singleProduct }
